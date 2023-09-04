bf2b707b7a983eb99dba61068c9af87ae147d8aa
Camillarhi <rhitanene@gmail.com>
2023-09-04 17:31:32 +0100

    added more unit tests

--- src/tests/components/Shareables/Spinner.test.tsx
+++ src/tests/components/Shareables/Spinner.test.tsx
@@ -1,40 +1,40 @@
-import React from 'react';
-import { render } from '@testing-library/react';
+import React from 'react'
+import { render } from '@testing-library/react'
 import Spinner from '../../../components/Shareables/Spinner'
 
 describe('Spinner component', () => {
   it('renders with default medium size', () => {
-    const { container } = render(<Spinner size='medium' />);
-    const spinnerElement = container.querySelector('.w-8.h-8'); // Assuming 'w-8 h-8' is the class for medium size
+    const { container } = render(<Spinner size='medium' />)
+    const spinnerElement = container.querySelector('.w-8.h-8') // Assuming 'w-8 h-8' is the class for medium size
 
-    expect(spinnerElement).toBeInTheDocument();
-  });
+    expect(spinnerElement).toBeInTheDocument()
+  })
 
   it('renders with small size', () => {
-    const { container } = render(<Spinner size="small" />);
-    const spinnerElement = container.querySelector('.w-4.h-4');
+    const { container } = render(<Spinner size='small' />)
+    const spinnerElement = container.querySelector('.w-4.h-4')
 
-    expect(spinnerElement).toBeInTheDocument();
-  });
+    expect(spinnerElement).toBeInTheDocument()
+  })
 
   it('renders with large size', () => {
-    const { container } = render(<Spinner size="large" />);
-    const spinnerElement = container.querySelector('.w-16.h-16');
+    const { container } = render(<Spinner size='large' />)
+    const spinnerElement = container.querySelector('.w-16.h-16')
 
-    expect(spinnerElement).toBeInTheDocument();
-  });
+    expect(spinnerElement).toBeInTheDocument()
+  })
 
   it('renders with a role attribute', () => {
-    const { container } = render(<Spinner size='small' />);
-    const spinnerElement = container.querySelector('[role="status"]');
+    const { container } = render(<Spinner size='small' />)
+    const spinnerElement = container.querySelector('[role="status"]')
 
-    expect(spinnerElement).toBeInTheDocument();
-  });
+    expect(spinnerElement).toBeInTheDocument()
+  })
 
   it('renders with a "Loading..." label', () => {
-    const { getByText } = render(<Spinner size='medium' />);
-    const loadingLabel = getByText('Loading...');
+    const { getByText } = render(<Spinner size='medium' />)
+    const loadingLabel = getByText('Loading...')
 
-    expect(loadingLabel).toBeInTheDocument();
-  });
-});
+    expect(loadingLabel).toBeInTheDocument()
+  })
+})
